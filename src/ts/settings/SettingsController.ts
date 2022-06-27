a26fd3f69d5f1c14210d616ff0e5e0b5260d047b
Ava Mattie <6314286+ava-cassiopeia@users.noreply.github.com>
2022-06-26 19:37:07 -0600

    Start to wire up settings

--- src/ts/settings/SettingsController.ts
+++ src/ts/settings/SettingsController.ts
@@ -1,3 +1,5 @@
+import {SiteSettings, SiteSettingsManifest} from "./SiteSettings";
+
 /**
  * Controller which manages the settings button and the settings dialog. Also
  * notifies other components when the settings are changed.
@@ -6,26 +8,60 @@ export class SettingsController {
 
   private readonly settingsDialog: HTMLDialogElement;
   private readonly settingsButton: HTMLElement;
+  private readonly settingsDialogForm: HTMLFormElement;
+  // Settings elements and inputs
+  private readonly showVisualizationCheckbox: HTMLInputElement;
 
   constructor() {
     this.settingsDialog =
       document.getElementById("settingsDialog") as HTMLDialogElement;
     this.settingsButton = document.getElementById("settingsButton");
+    this.showVisualizationCheckbox =
+        document.getElementById("showVisualizationCheckbox") as HTMLInputElement;
+    this.settingsDialogForm =
+        document.getElementById("settingsDialogForm") as HTMLFormElement;
 
     this.settingsButton.addEventListener(
       "click",
-      () => this.onSettingsButtonClick());
+      () => this.onSettingsButtonClick(),
+    );
+    this.settingsDialogForm.addEventListener(
+      "submit",
+      () => this.onSettingsFormSubmitted(),
+    );
+
+    // Initially load all settings
+    SiteSettings.loadAll();
   }
 
   private onSettingsButtonClick() {
     this.openSettings();
   }
 
+  private onSettingsFormSubmitted() {
+    this.syncSettingsFromDOM();
+  }
+
   private openSettings() {
+    this.syncSettingsToDOM();
     // @ts-ignore: .show() is flagged as not being part of a HTMLDialogElement,
     // but it is part of the specification as noted on MDN:
     // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/dialog
     this.settingsDialog.show();
+  }
+
+  private syncSettingsToDOM() {
+    this.showVisualizationCheckbox.checked =
+        Boolean(SiteSettingsManifest.SHOW_VISUALIZATION.get());
+  }
+
+  private syncSettingsFromDOM() {
+    const formData = new FormData(this.settingsDialogForm);
+    
+    // Checkbox values only show up if they are checked, so we can simply ask
+    // if the value was even provided at all.
+    SiteSettingsManifest.SHOW_VISUALIZATION.set(
+      formData.has("showVisualization"));
   }
 
   static init(): SettingsController {
--- src/ts/settings/SiteSettings.ts
+++ src/ts/settings/SiteSettings.ts
@@ -1,8 +1,123 @@
-/**
- * Represents and holds settings for the site. Includes functionality to
- * save these settings to the browser's Local Storage.
- */
-export class SiteSettings {
+export enum ColorModes {
+  FOLLOW_SYSTEM = "follow-system",
+  DARK_MODE = "dark",
+  LIGHT_MODE = "light",
+}
+
+export class SiteSettingsManifest {
+
+  static readonly SHOW_VISUALIZATION = new SiteSettingsManifest(
+    "show-visualization",
+    /* defaultValue= */ true,
+    // Decoder
+    (rawValue: string) => {
+      return rawValue.trim().toLowerCase() === "true";
+    },
+    // Encoder
+    (value: SiteSettingsValue) => {
+      return value ? "true" : "false";
+    },
+  );
+
+  static readonly INTERVAL_TIME_MS = new SiteSettingsManifest(
+    "interval-time",
+    /* defaultValue= */ 750,
+    // Decoder
+    (rawValue: string) => {
+      return parseInt(rawValue);
+    },
+    // Encoder
+    (value: SiteSettingsValue) => String(value),
+  );
+
+  static readonly CLICK_SOUND = new SiteSettingsManifest(
+    "click-sound",
+    /* defaultValue= */ "chime",
+    // Decoder
+    (rawValue: string) => rawValue,
+    // Encoder
+    (value: SiteSettingsValue) => String(value),
+  );
+
+  static readonly COLOR_MODE = new SiteSettingsManifest(
+    "color-mode",
+    /* defaultValue= */ ColorModes.FOLLOW_SYSTEM,
+    // Decoder
+    (rawValue: string): SiteSettingsValue => {
+      switch (rawValue) {
+        case ColorModes.DARK_MODE:
+          return ColorModes.DARK_MODE;
+        case ColorModes.FOLLOW_SYSTEM:
+          ColorModes.FOLLOW_SYSTEM;
+        case ColorModes.LIGHT_MODE:
+          return ColorModes.LIGHT_MODE;
+        default:
+          return ColorModes.FOLLOW_SYSTEM;
+      }
+    },
+    // Encoder
+    (value: SiteSettingsValue) => String(value),
+  );
+
+
+  private value: SiteSettingsValue;
+
+  private constructor(
+      readonly id: string,
+      readonly defaultValue: SiteSettingsValue,
+      private readonly decoder: SiteSettingsManifestDecoder,
+      private readonly encoder: SiteSettingsManifestEncoder) {
+    this.value = defaultValue;
+  }
+
+  /**
+   * Attempts to load a value from local storage.
+   */
+  load() {
+    this.value = this.getFromLocalStorage();
+  }
+
+  get(): SiteSettingsValue {
+    return this.value;
+  }
+
+  set(newValue: SiteSettingsValue) {
+    this.value = newValue;
+    this.sync();
+  }
+
+  reset() {
+    this.set(this.defaultValue);
+  }
+
+  private sync() {
+    localStorage.setItem(this.id, this.encoder(this.value));
+  }
+
+  private getFromLocalStorage(): SiteSettingsValue {
+    if (!window.localStorage) return this.defaultValue;
+    if (localStorage.getItem(this.id) == null) return this.defaultValue;
+    return this.decoder(localStorage.getItem(this.id));
+  }
 
 }
 
+export class SiteSettings {
+
+  private constructor() {}
+
+  /**
+   * Loads all site settings from local storage if possible.
+   */
+  static loadAll() {
+    SiteSettingsManifest.SHOW_VISUALIZATION.load();
+    SiteSettingsManifest.INTERVAL_TIME_MS.load();
+    SiteSettingsManifest.CLICK_SOUND.load();
+    SiteSettingsManifest.COLOR_MODE.load();
+  }
+
+}
+
+type SiteSettingsManifestDecoder = (rawValue: string) => SiteSettingsValue;
+type SiteSettingsManifestEncoder = (value: SiteSettingsValue) => string;
+type SiteSettingsValue = string|number|boolean|ColorModes;
